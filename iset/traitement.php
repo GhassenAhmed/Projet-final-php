d0e8019ebcca42703e1424cbd49b27a874e0dbdd
ghassen <ghassenahmed74@gmail.com>
2022-11-11 22:12:28 +0100

    add card

---
+++ iset/traitement.php
@@ -0,0 +1,53 @@
+<?php
+
+<div class="row" style='margin-top:60px;margin-left:50px'>
+<div class="col-sm col-md-3 col-xxl-3">
+    <div class="card" style="width: 15rem;height:20rem;text-align:center;">
+        <img src="../assets/departement.jpg" class="card-img-top" height="300px">
+            <div class="card-body">
+                <h5 class="card-title">Departement</h5>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+    </div>
+</div>
+
+<div class="col-sm">
+    <div class="card" style="width: 15rem;height:20rem;text-align:center;">
+            <img src="../assets/etudiants.jpg" class="card-img-top" height="300px" >
+            <div class="card-body">
+                <h5 class="card-title">Etudiants</h5>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+    </div>
+</div>
+
+<div class="col-xl-3">
+         <div class="card" style="width: 15rem; height:20rem;text-align:center;">
+            <img src="../assets/prof.jpg" class="card-img-top" height="300px">
+            <div class="card-body">
+                <h5 class="card-title">Enseingants</h5>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+</div>
+
+<div class="col-xl-3">
+         <div class="card" style="width: 15rem; height:20rem;text-align:center;">
+            <img src="../assets/prof.jpg" class="card-img-top" height="300px">
+            <div class="card-body">
+                <h5 class="card-title">Enseingants</h5>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+</div>
+
+<div class="col-xl-3">
+         <div class="card" style="width: 15rem; height:20rem;text-align:center;">
+            <img src="../assets/prof.jpg" class="card-img-top" height="300px">
+            <div class="card-body">
+                <h5 class="card-title">Enseingants</h5>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+</div>
+</div>
